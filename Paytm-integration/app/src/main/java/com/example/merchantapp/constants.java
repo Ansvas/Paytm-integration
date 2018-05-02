620810c479cba758e2ebb48317b504ac6460cdb8
Ansvas <anshul60198@gmail.com>
2018-05-02 20:37:31 +0530

    first commit

---
+++ Paytm-integration/app/src/main/java/com/example/merchantapp/constants.java
@@ -0,0 +1,18 @@
+package com.example.merchantapp;
+
+/**
+ * Created by Shiva on 01/05/2018.
+ */
+
+public class constants {
+    public static final String M_ID = "DIY12386817555501617"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WEB/WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
+
+
+
